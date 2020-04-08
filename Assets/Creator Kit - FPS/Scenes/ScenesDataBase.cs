0d7a2c637abac3b0e941a0e22dae1edfd62463d2
Amel-unity <amel.negra@unity3d.com>
2020-04-08 18:31:42 +0200

    Main menu script with loading screen

---
+++ Assets/Creator Kit - FPS/Scenes/ScenesDataBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ScenesDataBase : MonoBehaviour
+{
+    public static ScenesDataBase instance;
+    public string[] scenesNames;
+    void Awake()
+    {
+        instance = this;
+    }
+
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
+public class MainMenu : MonoBehaviour
+{
+    public GameObject Menu;
+    public Image LoadingScreenBackground;
+    public Image loadingProgressBar;
+
+    public void StartGame()
+    {
+        HideMenu();
+        ShowLoadingScreen();
+        StartCoroutine(LoadingScreen());
+    }
+
+    public void HideMenu()
+    {
+        Menu.SetActive(false);
+    }
+
+    public void ShowLoadingScreen()
+    {
+        LoadingScreenBackground.gameObject.SetActive(true);
+        loadingProgressBar.gameObject.SetActive(true);
+    }
+
+     
+    IEnumerator LoadingScreen()
+    {
+        AsyncOperation startLevel = SceneManager.LoadSceneAsync(ScenesDataBase.instance.scenesNames[1]);
+        loadingProgressBar.fillAmount = startLevel.progress;
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
