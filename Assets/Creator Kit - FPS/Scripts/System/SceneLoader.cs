b73a60fd48994b6cb585cd56d32f4d00b3c30b7b
Amel-unity <amel.negra@unity3d.com>
2020-04-17 17:16:26 +0200

    Verify which scenes are loaded on start to be able to make quick tests

--- Assets/Creator Kit - FPS/Scripts/System/SceneLoader.cs
+++ Assets/Creator Kit - FPS/Scripts/System/SceneLoader.cs
@@ -10,20 +10,44 @@ public class SceneLoader : MonoBehaviour
     private Transform[] levelScenesPos;
     private string[] levelScenes;
     private bool[] sceneLoaded;
-
     void Start()
     {
         levelScenesPos = ScenesDataBase.instance.levelScenesPositions;
         levelScenes = ScenesDataBase.instance.levelScenesNames;
         sceneLoaded = ScenesDataBase.instance.SceneState;
+
+        //verify if any scene is already open to avoid opening a scene twice
+        if (SceneManager.sceneCount > 0)
+        {
+            for (int i = 0; i < SceneManager.sceneCount; ++i)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+
+                if (scene.isLoaded)
+                {
+                    for (int j = 0; j < levelScenes.Length; ++j)
+                    {
+                        if(scene.name == levelScenes[j])
+                        {
+                            sceneLoaded[j] = true;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     void Update()
     {
-       
+      
         for (int i = 0; i < levelScenesPos.Length; ++i)
         {
-            if (Vector3.Distance(player.position, levelScenesPos[i].position) < loadRange){
+           
+            //Debug.Log(SceneManager.GetSceneAt(i).name);
+
+            if (Vector3.Distance(player.position, levelScenesPos[i].position) < loadRange)
+            {
+
                 if (!sceneLoaded[i])
                 {
                     SceneManager.LoadSceneAsync(levelScenes[i], LoadSceneMode.Additive);
@@ -38,6 +62,7 @@ public class SceneLoader : MonoBehaviour
                     sceneLoaded[i] = false;
                 }
             }
+
         }    
     }
 }
