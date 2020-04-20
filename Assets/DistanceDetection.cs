e2c83fb2f43ff972b4df7111ff9fafdab47fa8aa
Amel-unity <amel.negra@unity3d.com>
2020-04-20 14:13:21 +0200

    Now Possible to add a different range for each position

---
+++ Assets/DistanceDetection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDetection : MonoBehaviour
+{
+    public float loadRange;
+  
+    public float GetLoadRange()
+    {
+        return loadRange;
+    }
+}
