99f6c5bf629b5e5ce7cea4414fc64eb85e0c9b77
Amel-unity <amel.negra@unity3d.com>
2020-04-23 16:28:26 +0200

    Did I just redo the whole project in one better script? yes

---
+++ Assets/ScenePartLoader.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum CheckMethod
+{
+    Distance,
+    Trigger
+}
+public class ScenePartLoader : MonoBehaviour
+{
+
+    public Transform player;
+    public CheckMethod checkMethod;
+    public float loadRange;
+
+    //Scene state
+    private bool isLoaded;
+    private bool shouldLoad;
+    void Start()
+    {
+        //verify if the scene is already open to avoid opening a scene twice
+        if (SceneManager.sceneCount > 0)
+        {
+            for (int i = 0; i < SceneManager.sceneCount; ++i)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+
+                if (scene.isLoaded)
+                {
+                    if (scene.name == gameObject.name)
+                    {
+                        isLoaded = true;
+                    }
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (checkMethod == CheckMethod.Distance)
+        {
+            DistanceCheck();
+        }
+        else if (checkMethod == CheckMethod.Trigger)
+        {
+            TriggerCheck();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shouldLoad = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shouldLoad = false;
+        }
+    }
+
+    void TriggerCheck()
+    {
+        if (shouldLoad)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnLoadScene();
+        }
+    }
+
+    void DistanceCheck()
+    {
+        if (Vector3.Distance(player.position, transform.position) < loadRange)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnLoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if (!isLoaded)
+        {
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            isLoaded = true;
+        }
+    }
+
+    void UnLoadScene()
+    {
+        if (isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(gameObject.name);
+            isLoaded = false;
+        }
+    }
+}
+
